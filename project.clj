c245a014e6f470ac436111729ee456fc90844627
Jeff Weiss <jweiss@redhat.com>
2012-05-10 14:39:02 -0400

    added project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject com.redhat.qe/extended-sahi "1.0.2"
+  :description "An extension for Sahi web ui driver"
+  :java-source-path "src"
+  :dependencies [[sahi "3.5"]]
+  :javac-options {:debug "on"})
